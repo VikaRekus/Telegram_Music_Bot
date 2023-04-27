f045776cda69ed4d99d90275e237a9475ba63596
VikaRekus <118963270+VikaRekus@users.noreply.github.com>
2023-04-27 19:53:45 +0300

    Update bot_proect.py

--- bot_proect.py
+++ bot_proect.py
@@ -1,123 +1,191 @@
-import re
-import os
-from moviepy.editor import *
 import telebot
 import sqlite3
 import random
 from telebot import types
-#from selenium import webdriver
-import time
 from youtubesearchpython import VideosSearch
 from pytube import YouTube
-#import YouTubeMusicAPI
 
-#driver = webdriver.Chrome()
+
 bot = telebot.TeleBot('6077503747:AAG2VNsh3fXjhFrENc7gLze7mAJ7H5zibpA')
 
-
+#обработка команды старт
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    btn1 = types.KeyboardButton("👋 Поздороваться")
-    btn2 = types.KeyboardButton("🎶 Найти музыку")
-    btn3 = types.KeyboardButton("❓ Задать вопрос")
-    markup.add(btn1, btn2, btn3)
-    bot.send_message(message.chat.id,
-                     text="Привет, {0.first_name}! Я помогу тебе найти нужную музыку".format(message.from_user),
+
+    button1 = types.KeyboardButton("👋 Привет")
+    button2 = types.KeyboardButton("🎶 Найти музыку")
+    button3 = types.KeyboardButton("❓ Задать вопрос")
+
+    markup.add(button1, button2, button3)
+
+    bot.send_message(message.chat.id, text="Привет, {0.first_name}! Я помогу тебе найти нужную музыку".format(
+                     message.from_user),
                      reply_markup=markup)
 
 
+#обработка команды помощь
+@bot.message_handler(commands=['help'])
+def help(message):
+    returned(message)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+    bot.send_message(message.chat.id, text="{0.first_name}! Я помогу тебе разобраться с управлением и выяснить в чем проблема.".format(
+                     message.from_user),
+                     reply_markup=markup)
+    bot.send_message(message.chat.id, text="Если вы здесь, потому что не знаете как работает бот, вам сюда 👉🏻 /control")
+    bot.send_message(message.chat.id, text="Если у вас не загружается трек, вам сюда 👉 /error")
+
+
+#обработка команды помощи с ошибками
+@bot.message_handler(commands=['error'])
+def error(message):
+    returned(message)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+    bot.send_message(message.chat.id, text="{0.first_name}! Если у вас не загружается трек, то можно попробовать это исправить тем, что 👉".format(
+                     message.from_user),
+                     reply_markup=markup)
+    bot.send_message(message.chat.id, text="1 - Попробовать скачать трек снова")
+    bot.send_message(message.chat.id, text="2 - Попробовать скачать трек через некоторое время")
+    bot.send_message(message.chat.id, text="Если трек всё равно не скачался, то скорее всего это из-за ограничений по возрасту или слишком большого размера.😞 "
+                                           "Поэтому попробуйте скачать что-нибудь другое...")
+
+
+#обработка команды помощи с управлением
+@bot.message_handler(commands=['control'])
+def control(message):
+    returned(message)
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+    bot.send_message(message.chat.id, text="{0.first_name}! Я помогу тебе разобраться с управлением".format(
+                     message.from_user),
+                     reply_markup=markup)
+    bot.send_message(message.chat.id, text="Я могу найти вам трек по названию, когда вы выберите 👉 🎶 Найти музыку 👉 Найти по названию 👉 и введете название")
+    bot.send_message(message.chat.id, text="Также я могу посоветовать вам песню из своих запасов, для этого выберите 👉 🎶 Найти музыку 👉 Песня на выбор бота 👉 и подождите")
+
+
+#обработка команд вводящихся с клавиатуры
 @bot.message_handler(content_types=['text'])
-def inline_key(message):
-    if message.text == "/start":
-        bot.send_message(message.from_user.id, 'Привет!))')
-    elif message.text == "👋 Поздороваться":
+def comands(message):
+    #обработка команды "Привет"
+    if message.text == "👋 Привет":
         bot.send_message(message.chat.id, text="Привеееет. Рад вас видеть)")
+
+    #Обработка команды "Задать вопрос"
     elif message.text == "❓ Задать вопрос":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("Как тебя зовут?")
-        btn2 = types.KeyboardButton("Что ты можешь?")
-        back = types.KeyboardButton("Вернуться в главное меню")
-        markup.add(btn1, btn2, back)
-        bot.send_message(message.chat.id, text="Задай мне вопрос", reply_markup=markup)
+
+        button1 = types.KeyboardButton("Как тебя зовут?")
+        button2 = types.KeyboardButton("Что ты можешь?")
+        back_button = types.KeyboardButton("Вернуться в главное меню")
+
+        markup.add(button1, button2, back_button)
+
+        bot.send_message(message.chat.id,
+                         text="Задай мне вопрос",
+                         reply_markup=markup)
+
+    #Обработка команды "Найти музыку"
     elif message.text == "🎶 Найти музыку":
         markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        btn1 = types.KeyboardButton("Найти по названию")
-        btn2 = types.KeyboardButton("Песня на выбор бота")
-        back = types.KeyboardButton("Вернуться в главное меню")
-        markup.add(btn1, btn2, back)
-        bot.send_message(message.chat.id, text="Выбери действие", reply_markup=markup)
 
+        button1 = types.KeyboardButton("Найти по названию")
+        button2 = types.KeyboardButton("Песня на выбор бота")
+        back_button = types.KeyboardButton("Вернуться в главное меню")
+
+        markup.add(button1, button2, back_button)
+
+        bot.send_message(message.chat.id,
+                         text="Выбери действие",
+                         reply_markup=markup)
+
+    #Обработка вопросов:
+
+    #Обработка 1го вопроса
     elif message.text == "Как тебя зовут?":
         bot.send_message(message.chat.id, "У меня нет имени....")
 
+    #Обработка 2го вопроса
     elif message.text == "Что ты можешь?":
-        bot.send_message(message.chat.id, text="Могу найти вам определенную песню, а могу порекомендовать свою😜")
+        bot.send_message(message.chat.id,
+                         text="Могу найти вам определенную песню, а могу порекомендовать свою😜")
 
+    #Обработка команд для поиска песен:
+
+    #Обработка команды для поиска песни по названию
     elif message.text == "Найти по названию":
         sm = bot.send_message(message.chat.id, "Введите название песни, которую хотите найти")
         bot.register_next_step_handler(sm, search)
 
+    #Обработка команды для генерации песни из базы данных
     elif message.text == "Песня на выбор бота":
         bot.send_message(message.chat.id, "Подождите немного...")
-        time.sleep(3)
+
         sqlite_connection = sqlite3.connect('MyDB3.db')
         cursor = sqlite_connection.cursor()
 
         sqlite_select_query = """SELECT * from links"""
         cursor.execute(sqlite_select_query)
         records = cursor.fetchall()
+
         downloaded =False
         while not downloaded:
             try:
                 row = random.choice(records)
                 downloading(row[1], message)
                 downloaded = True
+
             except:
                 print("Error", row[1])
 
-
-
+    #Обработка возращения в главное меню
     elif message.text == "Вернуться в главное меню":
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button1 = types.KeyboardButton("👋 Поздороваться")
-        button2 = types.KeyboardButton("🎶 Найти музыку")
-        button3 = types.KeyboardButton("❓ Задать вопрос")
-        markup.add(button1, button2, button3)
-        bot.send_message(message.chat.id, text="Вы вернулись в главное меню", reply_markup=markup)
+        returned(message)
+
+    #Обработка непонятных для бота команд
     else:
-        bot.send_message(message.chat.id, text="На такую команду я не запрограммирован((....")
+        bot.send_message(message.chat.id,
+                         text="Я не очень вас понял, повторите пожалуйста((....")
 
 
+#Функция для возвращения в главное меню
+def returned(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+
+    button1 = types.KeyboardButton("👋 Привет")
+    button2 = types.KeyboardButton("🎶 Найти музыку")
+    button3 = types.KeyboardButton("❓ Задать вопрос")
+
+    markup.add(button1, button2, button3)
+
+    bot.send_message(message.chat.id, "Вы вернулись в главное меню",
+                     reply_markup=markup)
+
+
+#Функция для поиска видео по названию в YouTube и его скачиванию
 def search(message):
-    bot.send_message(message.chat.id, "Ищем и скачиваем, подождите немного...")
-    #result = YouTubeMusicAPI.search(message.text)
-    #bot.send_message(message.chat.id, result["url"])
-    #downloading(result["url"])
-    # video_href = "https://www.youtube.com/results?search_query=" + message.text
-    # driver.get(video_href)
-    # sleep(2)
-    # videos = driver.find_elements(by="video-title")
     result = VideosSearch(message.text, limit=1).result()
+
     for i in range(len(result["result"])):
-        downloading(result["result"][i]["link"], message)
-    #    # bot.send_message(message.chat.id, videos[i].get_attribute("href"))
-    #    # bot.send_message(message.chat.id, result["result"][i]["link"])
-    #    if i == 1:
-    #        break
+        try:
+            bot.send_message(message.chat.id, "Скачиваем..."+ result["result"][i]["link"])
+            downloading(result["result"][i]["link"], message)
+
+        except:
+            bot.send_message(message.chat.id, "Не получилось скачать...😢")
+            bot.send_message(message.chat.id, "Чтобы узнать проблему попробуйте команду /help")
+            returned(message)
 
 
+#Функция для скачивания звука из видео с YouTube
 def downloading(link, message):
-    try:
-        bot.send_message(message.chat.id, 'Скачиваем... '+link)
-
-        my_video = YouTube(link)
-        path = my_video.streams.filter(only_audio=True)[0].download()
-        video = open(path, 'rb')
-        print(path)
-        bot.send_audio(message.chat.id, video)
-    except:
-        bot.send_message(message.chat.id, 'Не получилось скачать...😢, попробуйте что-то другое')
+    my_video = YouTube(link)
+    path = my_video.streams.filter(only_audio=True)[0].download()
+    video = open(path, 'rb')
+    print(path)
+    bot.send_audio(message.chat.id, video)
+    bot.send_message(message.chat.id, f"Успешно скачан.....")
 
 
 bot.polling()
